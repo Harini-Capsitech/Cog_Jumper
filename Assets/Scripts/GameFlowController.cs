d96e181379babce70ba7906d191078ad10a35ea8
Harini-capsitech <harini.kanumuri@capsitech.com>
2026-01-06 16:23:35 +0530

    updated camera follow.

--- Assets/Scripts/GameFlowController.cs
+++ Assets/Scripts/GameFlowController.cs
@@ -223,6 +223,9 @@ public class GameFlowController : MonoBehaviour
         AppManager.instance.GameOver();
         mainCam.transform.parent = null;
         player.gameObject.SetActive(false);
+        //player.enabled = false;
+        //player.GetComponent<Rigidbody>().isKinematic = true;
+
         GameOverUI.Instance.Show(score,bestScore);
     }
 }
